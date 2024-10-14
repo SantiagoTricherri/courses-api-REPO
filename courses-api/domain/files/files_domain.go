1e66fdbc3908da564d2bc9472f4a6a75e10a476d
SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>
2024-10-14 14:28:18 -0300

    Add files via upload

---
+++ courses-api/domain/files/files_domain.go
@@ -0,0 +1,12 @@
+package files
+
+/*
+// File representa un archivo cargado
+type File struct {
+	ID       int64  `bson:"id"`
+	Name     string `bson:"name"`
+	Content  []byte `bson:"content"`
+	UserID   int64  `bson:"user_id"`
+	CourseID int64  `bson:"course_id"`
+}
+*/
