d289a0bd7c8e39a61f370396c5c14a420efcc5dd
SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>
2024-10-14 14:27:13 -0300

    Add files via upload

---
+++ courses-api/DTOs/files/files_DTOs.go
@@ -0,0 +1,20 @@
+package files
+
+/*
+// CreateFileRequestDTO representa la solicitud para cargar un archivo
+type CreateFileRequestDTO struct {
+	Name     string `json:"name" binding:"required"`
+	Content  string `json:"content" binding:"required"` // Base64 string
+	UserID   int64  `json:"userId" binding:"required"`
+	CourseID int64  `json:"courseId" binding:"required"`
+}
+
+// FileResponseDTO representa la respuesta al obtener un archivo
+type FileResponseDTO struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Content  []byte `json:"content"`
+	UserID   int64  `json:"userId"`
+	CourseID int64  `json:"courseId"`
+}
+*/
